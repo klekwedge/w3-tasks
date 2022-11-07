83d06ef7a9e123d879dacb95c3365ccd6f945954
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-07 22:27:56 +0300

    feat: add a solution to task number 11

---
+++ 1. Basic/11/script.js
@@ -0,0 +1,8 @@
+"use strict";
+// Write a JavaScript exercise to get the extension of a filename.
+function getExtension(str) {
+    return str.split('.').pop();
+}
+console.log(getExtension('script.js'));
+// 140°F
+// 7.222222222222222°C
