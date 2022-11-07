f321da2bbc9d03ce1eda673ac1c17b083d7fca04
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-07 20:53:20 +0300

    feat: add a solution to task number 2

---
+++ 1. Basic/02/script.js
@@ -0,0 +1,18 @@
+"use strict";
+// Write a JavaScript program to get the current date.
+// Expected Output :
+// mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+function getCurrentDate() {
+    const date = new Date();
+    let day = date.getDate();
+    day = day < 10 ? `0${day}` : day;
+    console.log(`Current date is ${day}-${date.getMonth() + 1}-${date.getFullYear()}`);
+    console.log(`Current date is ${day}/${date.getMonth() + 1}/${date.getFullYear()}`);
+    console.log(`Current date is ${date.getMonth() + 1}-${day}-${date.getFullYear()}`);
+    console.log(`Current date is ${date.getMonth() + 1}/${day}/${date.getFullYear()}`);
+}
+getCurrentDate();
+// Current date is 07-11-2022
+// Current date is 07/11/2022
+// Current date is 11-07-2022
+// Current date is 11/07/2022
