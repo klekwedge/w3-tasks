7f940931f9f0361913a809a255e996fc6bd7c1ef
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-07 21:01:47 +0300

    feat: add a solution to task number 3

---
+++ 1. Basic/03/script.ts
@@ -0,0 +1,15 @@
+// Write a JavaScript program to find the area of a triangle
+// where lengths of the three of its sides are 5, 6, 7.
+
+function calcAreaTriangle(): void {
+  const a = 5;
+  const b = 6;
+  const c = 7;
+  const perimeter = (a + b + c) / 2;
+  const area = Math.sqrt(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c))
+  console.log(`Area: ${area}`)
+}
+
+calcAreaTriangle();
+
+// Area: 14.696938456699069
