5a246a4b95cd19792f65c8f80ea49b02227e136e
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-07 22:18:45 +0300

    feat: add a solution to task number 9

---
+++ 1. Basic/09/script.ts
@@ -0,0 +1,15 @@
+// Write a JavaScript program to calculate multiplication and division of two numbers
+
+function multiplicationAndDivisonOfTwoNumbers(): void {
+  const a = 10;
+  const b = 12;
+
+  console.log('Multiplication:', a * b);
+  console.log('Division:', a / b);
+}
+
+multiplicationAndDivisonOfTwoNumbers();
+
+
+// Multiplication: 120
+// Division: 0.8333333333333334
