308ec24c7fdcd22500e727722c13e3d313c753d7
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-07 21:40:37 +0300

    feat: add a solution to task number 8

---
+++ 1. Basic/08/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// Write a JavaScript program to calculate days left until next Christmas.
+function countDaysUntilChristmas() {
+    const currentYear = new Date();
+    const nextYear = new Date(currentYear.getFullYear() + 1, 0, 1);
+    console.log(Math.floor((+nextYear - +currentYear) / (1000 * 60 * 60 * 24)) +
+        ' days left until Christmas!');
+}
+countDaysUntilChristmas();
+// 54 days left until Christmas!
