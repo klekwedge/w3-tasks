bbeb7ed0df4022ad531b4813dd0c7a8800fad274
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-08 16:45:36 +0300

    feat: add a solution to task number 17

--- 1. Basic/17/script.js
+++ 1. Basic/17/script.js
@@ -1,13 +1,14 @@
 "use strict";
-// Write a JavaScript program to check whether a given integer is within 20 of 100 or 400
-function checkNum(num) {
-    return (num >= 20 && num <= 100) || num === 400;
+// Write a JavaScript program to check from two given integers,
+// whether one is positive and another one is negative
+function checkNums(a, b) {
+    return (a >= 0 && b < 0) || (a < 0 && b >= 0);
 }
-console.log(checkNum(400));
-console.log(checkNum(21));
-console.log(checkNum(5));
-console.log(checkNum(3));
+console.log(checkNums(-20, 1));
+console.log(checkNums(-1, 20));
+console.log(checkNums(-1, -2));
+console.log(checkNums(0, 0));
 // true
-// true 
+// true
 // false
 // false
