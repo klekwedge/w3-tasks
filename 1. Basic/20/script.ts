19a01adae403fa1e8347f6f81c887d554a327dbf
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-09 22:14:54 +0300

    feat: add a solution to task number 20

---
+++ 1. Basic/20/script.ts
@@ -0,0 +1,22 @@
+// Write a JavaScript program to create a new string from a
+// given string changing the position of first and last
+// characters. The string length must be greater than or equal to 1.
+
+function changePositionCharacters(str: string): string | null {
+  if (str.length < 1) {
+    return null;
+  }
+
+  const arr = str.split('');
+  [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
+
+  return arr.join('');
+}
+
+console.log(changePositionCharacters('Hello'));
+console.log(changePositionCharacters('Look'));
+console.log(changePositionCharacters('Python'));
+
+// oellH
+// kooL
+// nythoP
