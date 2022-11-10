9ba0e0f686ccca41361337e9ee20204981baa811
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-10 16:11:37 +0300

    feat: add a solution to task number 28

---
+++ 1. Basic/28/script.js
@@ -0,0 +1,11 @@
+"use strict";
+// Write a JavaScript program to find the largest of three given integers.
+function findLargestNum(a, b, c) {
+    return Math.max(a, b, c);
+}
+console.log(findLargestNum(1, 6, 7));
+console.log(findLargestNum(5341, 22, 4));
+console.log(findLargestNum(1, 1, 1));
+// 7
+// 5341
+// 1
