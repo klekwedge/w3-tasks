6970983bea3ab28a9e5087f39800e982d3242504
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-10 15:45:16 +0300

    feat: add a solution to task number 24

---
+++ 1. Basic/24/script.js
@@ -0,0 +1,14 @@
+"use strict";
+// Write a JavaScript program to check whether a string
+// starts with 'Java' and false otherwise.
+function checkString(str) {
+    return str.startsWith('Java');
+}
+console.log(checkString('Hello'));
+console.log(checkString('Java'));
+console.log(checkString('Java231'));
+console.log(checkString('1Java212121'));
+// false
+// true
+// true
+// false
