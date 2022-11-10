59941fa116ca95860d512f45d40630e9f55a5c58
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-10 15:50:14 +0300

    feat: add a solution to task number 25

---
+++ 1. Basic/25/script.ts
@@ -0,0 +1,19 @@
+// Write a JavaScript program to check whether two given
+// integer values are in the range 50..99 (inclusive).
+// Return true if either of them are in the said range
+
+function isNumbersInRange(a: number, b: number): boolean {
+  return (a >= 50 && a <= 99) || (b >= 50 && b <= 99);
+}
+
+console.log(isNumbersInRange(55, 111));
+console.log(isNumbersInRange(5312, 65));
+console.log(isNumbersInRange(55,55));
+console.log(isNumbersInRange(76, 99));
+console.log(isNumbersInRange(100, 2121));
+
+// true
+// true
+// true
+// true
+// false
