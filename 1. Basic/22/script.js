4b33a640e4cb66716b26f564a4efe094eb54eeb9
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-10 15:35:44 +0300

    feat: add a solution to task number 22

---
+++ 1. Basic/22/script.js
@@ -0,0 +1,16 @@
+"use strict";
+// Write a JavaScript program to check whether a given
+// positive number is a multiple of 3 or a multiple of 7.
+function checkMultiple(num) {
+    return num % 3 === 0 || num % 7 === 0;
+}
+console.log(checkMultiple(3));
+console.log(checkMultiple(123));
+console.log(checkMultiple(35));
+console.log(checkMultiple(1));
+console.log(checkMultiple(5));
+// true
+// true
+// true
+// false
+// false
