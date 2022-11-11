9d2470415048aba0fc7847b63d07ed61dc28c91c
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-11 11:49:58 +0300

    feat: add a solution to task number 40

---
+++ 1. Basic/40/script.js
@@ -0,0 +1,19 @@
+"use strict";
+// Write a JavaScript program to check from three given numbers
+// (non negative integers) that two or all of them have the
+// same rightmost digit.
+function checkNums(a, b, c) {
+    return a % 10 === b % 10 || a % 10 === c % 10 || b % 10 === c % 10;
+}
+console.log(checkNums(22, 22, 31));
+console.log(checkNums(24, 22, 31));
+console.log(checkNums(10, 15, 31));
+console.log(checkNums(24, 24, 24));
+console.log(checkNums(7, 4, 4));
+console.log(checkNums(12, 4, 1));
+// true
+// false
+// false
+// true
+// true
+// false
