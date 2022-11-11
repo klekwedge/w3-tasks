23e229d8c72e3816256b6473ce6a3494221d3de4
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-11 12:02:46 +0300

    feat: add a solution to task number 43

---
+++ 1. Basic/43/script.js
@@ -0,0 +1,18 @@
+"use strict";
+// Write a JavaScript program to check two given non-negative integers
+// that whether one of the number (not both) is multiple of 7 or 11.
+function checkNums(a, b) {
+    return (((a % 7 === 0 || a % 11 === 0) && !(b % 7 === 0 || b % 11 === 0)) ||
+        (!(a % 7 === 0 || a % 11 === 0) && (b % 7 === 0 || b % 11 === 0)));
+}
+console.log(checkNums(15, 10));
+console.log(checkNums(11, 11));
+console.log(checkNums(7, 15));
+console.log(checkNums(11, 65));
+console.log(checkNums(24, 11));
+// true
+// false
+// true
+// false
+// true
+// false
