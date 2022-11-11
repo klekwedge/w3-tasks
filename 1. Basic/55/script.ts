5c16ae1471d68c61ef5c8ff1e63ef0825e0a02f1
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-11 13:26:15 +0300

    feat: add a solution to task number 55

---
+++ 1. Basic/55/script.ts
@@ -0,0 +1,23 @@
+// Write a JavaScript program to create a new string of 4 copies
+// of the last 3 characters of a given original string. The length
+// of the given string must be 3 and above.
+
+function copyString(str: string): string {
+  if (str.length < 3) {
+    return '';
+  }
+
+  return str.slice(-3).repeat(4);
+}
+
+console.log(copyString('gfcnngndmm'));
+console.log(copyString('adc'));
+console.log(copyString('fff'));
+console.log(copyString('opopo'));
+console.log(copyString('test'));
+
+// dmmdmmdmmdmm
+// adcadcadcadc
+// ffffffffffff
+// opoopoopoopo
+// estestestest
