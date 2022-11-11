3f8dda0ad221165119364ecfcae8b26ec38b80ff
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-11 13:34:30 +0300

    feat: add a solution to task number 58

---
+++ 1. Basic/58/script.js
@@ -0,0 +1,16 @@
+"use strict";
+// Write a JavaScript program to concatenate two strings
+// except their first character.
+function createNewString(a, b) {
+    return a.slice(1) + b.slice(1);
+}
+console.log(createNewString('gfcnngndmm', 'fbn'));
+console.log(createNewString('adc2', 'ybvcgb'));
+console.log(createNewString('ffft', 'hgfbh'));
+console.log(createNewString('op6opo', 'jjvff'));
+console.log(createNewString('test', 'plknbg'));
+// fcnngndmmbn
+// dc2bvcgb
+// fftgfbh
+// p6opojvff
+// estlknbg
