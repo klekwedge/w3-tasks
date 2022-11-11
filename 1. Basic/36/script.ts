88513050663b19d02ae940f13cc7f4a0f345e69c
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-11 11:44:01 +0300

    feat: add a solution to task number 39

--- 1. Basic/36/script.ts
+++ 1. Basic/36/script.ts
@@ -1,6 +1,6 @@
 // Write a JavaScript program to compute the sum of the two given
 // integers, If the sum is in the range 50..80 return 65 other
-//wise return 80.
+// wise return 80.
 
 function computeSum(a: number, b: number): number {
   return a + b >= 50 && a + b <= 80 ? 65 : 80
---
+++ 1. Basic/39/script.ts
@@ -0,0 +1,26 @@
+// Write a JavaScript program to check whether three given numbers
+// are increasing in strict mode or in soft mode.  Go to the editor
+// Note: Strict mode -> 10, 15, 31 : Soft mode -> 24, 22, 31 or 22, 22, 31
+
+function checkNums(a: number, b: number, c: number): string | null {
+  if (c >= b && b >= a) {
+    return 'Strict mode';
+  } else if (c >= b) {
+    return 'Soft mode';
+  }
+  return null;
+}
+
+console.log(checkNums(22, 22, 31));
+console.log(checkNums(24, 22, 31));
+console.log(checkNums(10, 15, 31));
+console.log(checkNums(7, 4, 4));
+console.log(checkNums(12, 4, 1));
+console.log(checkNums(9, 65, 343));
+
+// Strict mode
+// Soft mode
+// Strict mode
+// Soft mode
+// null
+// Strict mode
