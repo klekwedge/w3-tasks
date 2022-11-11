9701365c01138f5440d883a09de09612a573f6c6
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-11 12:05:04 +0300

    feat: add a solution to task number 44

---
+++ 1. Basic/44/script.ts
@@ -0,0 +1,19 @@
+// Write a JavaScript program to check whether a given number
+// is presents in the range 40..10000.
+// For example 40 presents in 40 and 4000
+
+function checkNum(a: number): boolean {
+  return a >= 40 && a <= 10000;
+}
+
+console.log(checkNum(15));
+console.log(checkNum(11));
+console.log(checkNum(5000));
+console.log(checkNum(65));
+console.log(checkNum(1111111111));
+
+// false
+// false
+// true
+// true
+// false
