d8a6a229fe32e6073f51a7c8071db76b8746458d
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-11 13:40:10 +0300

    feat: add a solution to task number 59

---
+++ 1. Basic/59/script.js
@@ -0,0 +1,19 @@
+"use strict";
+// Write a JavaScript program to move last three character to the
+// start of a given string. The string length must be greater or equal to three
+function createNewString(str) {
+    if (str.length < 3) {
+        return str;
+    }
+    return str.slice(-3) + str.slice(0, -3);
+}
+console.log(createNewString('abcf'));
+console.log(createNewString('adc2'));
+console.log(createNewString('fetwmcotfft'));
+console.log(createNewString('tovntvnrhyrtew'));
+console.log(createNewString('test'));
+// bcfa
+// dc2a
+// fftfetwmcot
+// tewtovntvnrhyr
+// estt
