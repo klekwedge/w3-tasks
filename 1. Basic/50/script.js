e4448c128c12c1bedb09efac16266d4ccf4a7d3a
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-11 13:04:41 +0300

    feat: add a solution to task number 50

---
+++ 1. Basic/50/script.js
@@ -0,0 +1,16 @@
+"use strict";
+// Write a JavaScript program to check whether the characters a and
+// b are separated by exactly 3 places anywhere (at least once) in a given string.
+function convertLetters(str) {
+    return /a...b/.test(str) || /b...a/.test(str);
+}
+console.log(convertLetters('af21b'));
+console.log(convertLetters('fkvlclaggggggggbrorpdkmcmcx'));
+console.log(convertLetters('vrlea---blwkrkwca---bmdmwcdw'));
+console.log(convertLetters('fkelwcafffbfvmevrm'));
+console.log(convertLetters('gkldlcmfkrd'));
+// true
+// false
+// true
+// true
+// false
