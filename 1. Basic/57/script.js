97c50cd7cf5e02d46e1149e766788cfae84b6d19
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-11 13:31:36 +0300

    feat: add a solution to task number 57

---
+++ 1. Basic/57/script.js
@@ -0,0 +1,16 @@
+"use strict";
+// Write a JavaScript program to create a new string without
+// the first and last character of a given string.
+function createNewString(str) {
+    return str.slice(1, str.length - 1);
+}
+console.log(createNewString('gfcnngndmm'));
+console.log(createNewString('adc2'));
+console.log(createNewString('ffft'));
+console.log(createNewString('op6opo'));
+console.log(createNewString('test'));
+// fcnngndm
+// dc
+// ff
+// p6op
+// es
