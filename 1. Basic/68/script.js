29d6005b8aab143bd79ff18c236e651ab282033f
Andrey <antonov.andrey.10101@gmail.com>
2022-12-13 10:23:09 +0300

    feat: add a solution to task number 68

--- 1. Basic/68/script.js
+++ 1. Basic/68/script.js
@@ -2,19 +2,19 @@
 // Write a JavaScript program to check whether 1 appears in
 // first or last position of a given array of integers. The
 // array length must be greater or equal to 1.
-function calcSum(arr) {
+function checkEl(arr) {
     if (arr.length < 1) {
         return false;
     }
     return arr[0] === 1 || arr[arr.length - 1] === 1;
 }
-console.log(calcSum([1, 5, 6]));
-console.log(calcSum([16, 1, 3]));
-console.log(calcSum([1, 0, 1]));
-console.log(calcSum([0, 2, 3]));
-console.log(calcSum([6, 1, 6]));
-// [ 5, 6, 1 ]
-// [ 1, 1, 16 ]
-// [ 0, 0, 0 ]
-// [ 2, 3, 0 ]
-// [ 1, 6, 6 ]
+console.log(checkEl([1, 5, 6]));
+console.log(checkEl([16, 1, 3]));
+console.log(checkEl([1, 0, 1]));
+console.log(checkEl([0, 2, 3]));
+console.log(checkEl([6, 1, 6]));
+// true
+// false
+// true
+// false
+// false
--- 1. Basic/68/script.ts
+++ 1. Basic/68/script.ts
@@ -2,18 +2,18 @@
 // first or last position of a given array of integers. The
 // array length must be greater or equal to 1.
 
-function calcSum(arr: number[]): boolean {
+function checkEl(arr: number[]): boolean {
   if (arr.length < 1) {
     return false;
   }
   return arr[0] === 1 || arr[arr.length - 1] === 1;
 }
 
-console.log(calcSum([1, 5, 6]));
-console.log(calcSum([16, 1, 3]));
-console.log(calcSum([1, 0, 1]));
-console.log(calcSum([0, 2, 3]));
-console.log(calcSum([6, 1, 6]));
+console.log(checkEl([1, 5, 6]));
+console.log(checkEl([16, 1, 3]));
+console.log(checkEl([1, 0, 1]));
+console.log(checkEl([0, 2, 3]));
+console.log(checkEl([6, 1, 6]));
 
 // true
 // false
