48244e129d89d907c6c55f23a64ef9863193d703
Andrey <antonov.andrey.10101@gmail.com>
2022-12-13 10:22:12 +0300

    feat: add a solution to task number 67

--- 1. Basic/67/script.js
+++ 1. Basic/67/script.js
@@ -1,14 +1,14 @@
 "use strict";
 // Write a JavaScript program to rotate the elements
 // left of a given array of integers of length 3
-function calcSum(arr) {
+function rotateElements(arr) {
     return [arr[1], arr[2], arr[0]];
 }
-console.log(calcSum([1, 5, 6]));
-console.log(calcSum([16, 1, 1]));
-console.log(calcSum([0, 0, 0]));
-console.log(calcSum([0, 2, 3]));
-console.log(calcSum([6, 1, 6]));
+console.log(rotateElements([1, 5, 6]));
+console.log(rotateElements([16, 1, 1]));
+console.log(rotateElements([0, 0, 0]));
+console.log(rotateElements([0, 2, 3]));
+console.log(rotateElements([6, 1, 6]));
 // [ 5, 6, 1 ]
 // [ 1, 1, 16 ]
 // [ 0, 0, 0 ]
