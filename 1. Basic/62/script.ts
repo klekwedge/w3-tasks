8f33ebf80e4257bb0275845141e391090fdf7551
Andrey <antonov.andrey.10101@gmail.com>
2022-12-13 09:23:25 +0300

    feat: add a solution to task number 62

---
+++ 1. Basic/62/script.ts
@@ -0,0 +1,21 @@
+// Write a JavaScript program to test whether a string end with "Script".
+// The string length must be greater or equal to 6
+function testString(str: string): boolean {
+  if (str.length < 6) {
+    return false;
+  }
+
+  return /Script$/g.test(str)
+}
+
+console.log(testString("abcffScript"));
+console.log(testString("tovntv5nrhyrtew"));
+console.log(testString("Scripttovntv5nrhyrtew"));
+console.log(testString("teScriptst5"));
+console.log(testString("t65e4johb4Script53vest5"));
+
+// true
+// false
+// false
+// false
+// false
