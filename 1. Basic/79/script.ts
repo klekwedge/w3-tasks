11efc14b114c374b79dfa3c998d2dc7df4bfd644
Andrey <antonov.andrey.10101@gmail.com>
2022-12-13 12:12:23 +0300

    feat: add a solution to task number 79

---
+++ 1. Basic/79/script.ts
@@ -0,0 +1,25 @@
+// Write a JavaScript to add two positive integers without carry.
+
+function addTwoIntegersWithoutCarry(a: number, b: number): number {
+  let res = 0;
+  let count = 1;
+  while (a / 10 !== 0 && b / 10 !== 0) {
+    res += count * (((a % 10) + (b % 10)) % 10);
+    a = Math.floor(a / 10);
+    b = Math.floor(b / 10);
+    count *= 10;
+  }
+  return res;
+}
+
+console.log(addTwoIntegersWithoutCarry(222, 911));
+console.log(addTwoIntegersWithoutCarry(200, 900));
+console.log(addTwoIntegersWithoutCarry(413, 111));
+console.log(addTwoIntegersWithoutCarry(632, 241));
+console.log(addTwoIntegersWithoutCarry(421, 425));
+
+// 133
+// 100
+// 524
+// 873
+// 846
