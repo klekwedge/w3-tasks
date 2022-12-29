d44f3da194450686aa340ea57d6d9c6dae6197b0
Andrey <antonov.andrey.10101@gmail.com>
2022-12-29 15:46:31 +0300

    feat: add a solution to task number 108

---
+++ 1. Basic/108/script.ts
@@ -0,0 +1,28 @@
+// Write a JavaScript program to create the dot products of two given
+// 3D vectors. Note: The dot product is the sum of the products
+// of the corresponding entries of the two sequences of numbers.
+
+function createDotProducts(a: number[], b: number[]) {
+  if (a.length !== 3 || b.length !== 3) {
+    return null;
+  }
+
+  let res = 0;
+  for (let i = 0; i < a.length; i++) {
+    res += a[i] * b[i];
+  }
+
+  return res;
+}
+
+console.log(createDotProducts([11, 23, 6], [1, 5, 4]));
+console.log(createDotProducts([1, 9, 7], [9, 1, 2]));
+console.log(createDotProducts([1, 2, 3], [1, 2, 3]));
+console.log(createDotProducts([2, 4, 6], [2, 4, 6]));
+console.log(createDotProducts([1, 1, 1], [0, 1, -1]));
+
+// 150
+// 32
+// 14
+// 56
+// 0
