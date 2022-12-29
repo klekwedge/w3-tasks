8582f0f70f019fff75c172801b52e53d6a006399
Andrey <antonov.andrey.10101@gmail.com>
2022-12-29 19:27:17 +0300

    feat: add a solution to task number 141

---
+++ 1. Basic/141/script.ts
@@ -0,0 +1,22 @@
+// Write a JavaScript program to find the number of elements which
+// presents in both of the given arrays.
+
+function testSameElemBothArrays(arr1: number[], arr2: number[]) {
+  const obj = { ...arr1 };
+  let counter = 0;
+  for (let i = 0; i < arr2.length; i++) {
+    const el = arr2[i];
+    if (obj.hasOwnProperty(String(el - 1))) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+console.log(testSameElemBothArrays([1, 2, 3, 4], [1, 2, 3, 4]));
+console.log(testSameElemBothArrays([1, 2, 3, 4], [1, 2, 3, 5]));
+console.log(testSameElemBothArrays([1, 2, 3, 4], [11, 22, 33, 44]));
+
+// 4
+// 3
+// 0
