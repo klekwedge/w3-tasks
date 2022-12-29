2faaca40e484decb6209c1f59004dd932196b469
Andrey <antonov.andrey.10101@gmail.com>
2022-12-29 16:58:57 +0300

    feat: add a solution to task number 119

---
+++ 1. Basic/119/script.js
@@ -0,0 +1,19 @@
+"use strict";
+// Write a JavaScript program to check whether a given integer has an increasing digits sequence.
+function isIncreasingDigitsSequence(num) {
+    const arr = String(num)
+        .split("")
+        .map((item) => +item);
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i + 1] <= arr[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isIncreasingDigitsSequence(123));
+console.log(isIncreasingDigitsSequence(1223));
+console.log(isIncreasingDigitsSequence(45677));
+// true
+// false
+// false
