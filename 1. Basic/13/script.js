31b366bee924edeee6886762c2fb46e5dfce038f
Andrey <antonov.andrey.10101@gmail.com>
2022-12-29 20:51:53 +0300

    feat: add a solution to task number 13

---
+++ 1. Basic/13/script.js
@@ -0,0 +1,8 @@
+"use strict";
+// Write a JavaScript exercise to create a variable using a user-defined name
+const obj = {
+    name: "abcd",
+    year: 30,
+};
+console.log(obj);
+// { name: 'abcd', year: 30 }
---
+++ 1. Basic/13/script.ts
@@ -0,0 +1,15 @@
+// Write a JavaScript exercise to create a variable using a user-defined name
+
+interface IObj {
+  name: string;
+  year: number;
+}
+
+const obj: IObj = {
+  name: "abcd",
+  year: 30,
+};
+
+console.log(obj);
+
+// { name: 'abcd', year: 30 }
