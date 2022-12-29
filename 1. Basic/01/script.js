75d3b4afe3c60e4f0629c64e7d4d9a44da786d03
Andrey <antonov.andrey.10101@gmail.com>
2022-12-29 20:32:30 +0300

    feat: add description for the 01 task

--- 1. Basic/01/script.js
+++ 1. Basic/01/script.js
@@ -1,20 +1,32 @@
 "use strict";
+// Write a JavaScript program to display the current day and time in the following format.
+// Sample Output : Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
 function getDay(day) {
     switch (day) {
-        case 0: return 'Sunday';
-        case 1: return 'Monday';
-        case 2: return 'Tuesday';
-        case 3: return 'Wednesday';
-        case 4: return 'Thursday';
-        case 5: return 'Friday';
-        case 6: return 'Saturday';
+        case 0:
+            return "Sunday";
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
         default:
-            return '';
+            return "";
     }
 }
 function getCurrentDay() {
     const date = new Date();
-    console.log('Today is', getDay(date.getDay()));
+    console.log("Today is", getDay(date.getDay()));
     console.log(`Current time is ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 }
 getCurrentDay();
+// Today is Thursday
+// Current time is 20:31:29
