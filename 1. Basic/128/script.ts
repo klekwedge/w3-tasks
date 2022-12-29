50c7e6a0d6f7ca3f2e935e8bee6836b844d2f8ec
Andrey <antonov.andrey.10101@gmail.com>
2022-12-29 18:37:21 +0300

    feat: add a solution to task number 128

---
+++ 1. Basic/128/script.ts
@@ -0,0 +1,19 @@
+// Write a JavaScript program to find the smallest round number that
+// is not less than a given value. Note: A round number is informally
+// considered to be an integer that ends with one or more zeros.
+// So, 590 is rounder than 592, but 590 is less round than 600.
+
+function nearestRoundNumber(num: number) {
+  while (num % 10) {
+    num++;
+  }
+  return num;
+}
+
+console.log(nearestRoundNumber(56));
+console.log(nearestRoundNumber(592));
+console.log(nearestRoundNumber(750));
+
+// 60
+// 600
+// 750
