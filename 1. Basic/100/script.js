79088d9a1f0f161506bb32d33a487a02c74fb972
Andrey <antonov.andrey.10101@gmail.com>
2022-12-29 09:31:28 +0300

    feat: add a solution to task number 100

---
+++ 1. Basic/100/script.js
@@ -0,0 +1,22 @@
+"use strict";
+// Write a JavaScript program to check whether there is at least
+// one element which occurs in two given sorted arrays of integers.
+function checkArrays(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        const elem = arr1[i];
+        if (arr2.find((i) => i === elem)) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log(checkArrays([1, 2, 3, 4, 5, 6, 7, 8], [8, 9, 10]));
+console.log(checkArrays([1, 4, 5, 6, 7, 8], [11, 12]));
+console.log(checkArrays([1, 6, 7, 8], [1, 8]));
+console.log(checkArrays([1, 2, 3, 4], [4, 5, 6]));
+console.log(checkArrays([4, 5, 6, 7, 8], [11, 22, 44]));
+// true
+// false
+// true
+// true
+// false
