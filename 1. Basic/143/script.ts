4a44310c6792fde655589708e68073fcd1e20f7b
Andrey <antonov.andrey.10101@gmail.com>
2022-12-29 19:31:25 +0300

    feat: add a solution to task number 143

---
+++ 1. Basic/143/script.ts
@@ -0,0 +1,41 @@
+// Write a JavaScript program to sort the strings of a given
+// array of strings in the order of increasing lengths.
+// Note: Do not change the order if the lengths of two string are same.
+
+function sortByStringLength(arr: string[]) {
+  return arr.sort((a, b) => a.length - b.length);
+}
+
+console.log(
+  sortByStringLength(["xyz", "acd", "aa", "bb", "zzz", "", "a", "b"])
+);
+console.log(
+  sortByStringLength([
+    "213x2",
+    "5c432x",
+    "323132",
+    "5yvhvfffv",
+    "fc",
+    "",
+    "a",
+    "b",
+  ])
+);
+console.log(
+  sortByStringLength(["21gcft", "acd", "c432", "bb", "5c234", "2121", "a", "b"])
+);
+
+// [
+//   '',    'a',   'b',
+//   'aa',  'bb',  'xyz',
+//   'acd', 'zzz'
+// ]
+
+// [ '', 'a', 'b', 'fc', '213x2', '5c432x', '323132', '5yvhvfffv' ]
+
+// [
+//   'a',     'b',
+//   'bb',    'acd',
+//   'c432',  '2121',
+//   '5c234', '21gcft'
+// ]
