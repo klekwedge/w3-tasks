c6554525af438266a419191dca550571feafac85
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 13:52:35 +0300

    feat: add a solution to task number 26

---
+++ 10. String/26/script.js
@@ -0,0 +1,16 @@
+"use strict";
+// Write a JavaScript function to remove the first occurrence of a given
+// 'search string' from a string.
+// Test Data :
+// console.log(remove_first_occurrence("The quick brown fox jumps over the lazy dog", 'the'));
+// Output :
+// "The quick brown fox jumps over lazy dog"
+function removeFirstOccurrence(str, occ) {
+    const occIndex = str.indexOf(occ);
+    if (occIndex === -1) {
+        return str;
+    }
+    return str.slice(0, occIndex) + str.slice(occIndex + occ.length);
+}
+console.log(removeFirstOccurrence("The quick brown fox jumps over the lazy dog", "the"));
+// The quick brown fox jumps over  lazy dog
