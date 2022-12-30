cdad7a1856341aea8fd98d398a3fd86a48eed61b
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 16:15:01 +0300

    feat: add a solution to task number 4

---
+++ 8. Array/04/script.ts
@@ -0,0 +1,26 @@
+// Write a JavaScript function to get the last element of an array.
+// Passing a parameter 'n' will return the last 'n' elements of the array.
+// Test Data :
+// console.log(last([7, 9, 0, -2]));
+// console.log(last([7, 9, 0, -2],3));
+// console.log(last([7, 9, 0, -2],6));
+// Expected Output :
+// -2
+// [9, 0, -2]
+// [7, 9, 0, -2]
+
+function last(arr: number[], num?: number) {
+  if (!num) {
+    return arr[arr.length - 1];
+  }
+
+  return arr.slice(Math.max(arr.length - num, 0));
+}
+
+console.log(last([7, 9, 0, -2]));
+console.log(last([7, 9, 0, -2], 3));
+console.log(last([7, 9, 0, -2], 6));
+
+// -2
+// [ 9, 0, -2 ]
+// [ 7, 9, 0, -2 ]
