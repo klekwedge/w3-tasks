8899c104ca7ac159c7d0d1504a2995f5245bbe96
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 10:49:13 +0300

    feat: add a solution to task number 4

---
+++ 4. Functions/04/script.js
@@ -0,0 +1,11 @@
+"use strict";
+// Write a JavaScript function that returns a passed string with letters in
+// alphabetical order.
+// Example string : 'webmaster'
+// Expected Output : 'abeemrstw'
+// Assume punctuation and numbers symbols are not included in the passed string.
+function alphabetOrder(str) {
+    return str.split('').sort().join('');
+}
+console.log(alphabetOrder("webmaster"));
+// abeemrstw
