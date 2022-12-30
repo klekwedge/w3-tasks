f951c3394ee6779f41f401c8a54f42aa486779f3
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 12:35:32 +0300

    feat: add a solution to task number 10

---
+++ 10. String/10/script.js
@@ -0,0 +1,21 @@
+"use strict";
+// Write a JavaScript function that takes a string which has lower and upper case
+// letters as a parameter and converts upper case letters to lower case, and lower
+// case letters to upper case.
+// Test Data :
+// console.log(swapcase('AaBbc'));
+// "aAbBC"
+function swapCase(str) {
+    let res = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].toUpperCase() === str[i]) {
+            res += str[i].toLowerCase();
+        }
+        else {
+            res += str[i].toUpperCase();
+        }
+    }
+    return res;
+}
+console.log(swapCase("AaBbc"));
+// aAbBC
