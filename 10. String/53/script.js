f3013a7d042a57c924b87d9e3eb820133896e135
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 15:33:15 +0300

    feat: add a solution to task number 53

---
+++ 10. String/53/script.js
@@ -0,0 +1,23 @@
+"use strict";
+// Write a JavaScript function to check a given string is in Flat case or not.
+// Flat case: As the name implies, flatcase refers to the use of lowercase
+// letters, with no spaces between words.
+// Test Data :
+// ('j') -> true
+// ('java exercises') -> false
+// ('JavaScriptExercises') -> false
+// ('javascriptexercises') -> true
+// (12356) -> "It must be a string."
+function test(value) {
+    return /^[a-z]*$/gi.test(String(value));
+}
+console.log(test('j'));
+console.log(test('java exercises'));
+console.log(test('JavaScriptExercises'));
+console.log(test('javascriptexercises'));
+console.log(test(12356));
+// true
+// false
+// true
+// true
+// false
