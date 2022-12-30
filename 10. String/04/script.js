7c1ba21d27041f0080a3c437159dd29bf6a5bf61
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 12:25:58 +0300

    feat: add description of test data for 4 task

--- 10. String/04/script.js
+++ 10. String/04/script.js
@@ -1,5 +1,8 @@
 "use strict";
 // Write a JavaScript function to check whether a string is blank or not.
+// Test Data :
+// console.log(truncate_string("Robin Singh",4));
+// "Robi"
 function truncateString(str, length) {
     if (typeof str === "string" && length > 0) {
         return str.slice(0, length);
--- 10. String/04/script.ts
+++ 10. String/04/script.ts
@@ -1,4 +1,7 @@
 // Write a JavaScript function to check whether a string is blank or not.
+// Test Data :
+// console.log(truncate_string("Robin Singh",4));
+// "Robi"
 
 function truncateString(str: string, length: number) {
   if (typeof str === "string" && length > 0) {
