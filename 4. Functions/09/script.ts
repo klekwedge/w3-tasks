e6982f9f39217cce0956d50640cf6264690fca9f
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 11:02:15 +0300

    feat: add a solution to task number 9

---
+++ 4. Functions/09/script.ts
@@ -0,0 +1,26 @@
+// Write a JavaScript function which accepts an argument and returns the type.
+// Note : There are six possible values that typeof returns: object, boolean,
+// function, number, string, and undefined.
+
+function detectDataType(value: any) {
+  const dtypes = [Function, RegExp, Number, String, Boolean, Object];
+  let len = dtypes.length
+
+  if (typeof value === "object" || typeof value === "function") {
+    for (let i = 0; i < len; i++) {
+      if (value instanceof dtypes[i]) {
+        return dtypes[i];
+      }
+    }
+  }
+
+  return typeof value;
+}
+
+console.log(detectDataType(12));
+console.log(detectDataType("w3resource"));
+console.log(detectDataType(false));
+
+// number
+// string
+// boolean
