bfd2be7faeb18680e53a619bb0e4b0e69eb92be8
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 15:09:04 +0300

    feat: add a solution to task number 40

---
+++ 10. String/40/script.ts
@@ -0,0 +1,15 @@
+// Write a JavaScript function to Uncapitalize the first letter of each word of a string.
+// Test Data :
+// console.log(unCapitalize_Words('Js String Exercises'));
+// "js string exercises
+
+function unCapitalizeWords(str: string) {
+  return str
+    .split(" ")
+    .map((item) => item[0].toLowerCase() + item.slice(1))
+    .join(" ");
+}
+
+console.log(unCapitalizeWords("Js String Exercises"));
+
+// js string exercises
