3955cd044d03abaa749c963ea72c5ad9c1799b1a
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 10:36:59 +0300

    feat: change solution for task 12

---
+++ 6. Conditions/12/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// Write a JavaScript program to sum the multiples of 3 and 5 under 1000.
+let sum = 0;
+for (let i = 0; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+    }
+}
+console.log(sum);
+// 233168
