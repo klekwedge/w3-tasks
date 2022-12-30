3f541b58266f0b711c3af3375d2f684c0b84e04c
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 12:13:41 +0300

    feat: add a solution to task number 3

---
+++ 10. String/03/script.ts
@@ -0,0 +1,9 @@
+// Write a JavaScript function to check whether a string is blank or not.
+
+function stringToArray(str: string) {
+  return str.trim().split(" ");
+}
+
+console.log(stringToArray("Robin Singh"));
+
+// [ 'Robin', 'Singh' ]
