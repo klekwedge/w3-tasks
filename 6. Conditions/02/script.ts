d3e308608e2aefe8a6da9128d5350d3fa16156f1
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 10:15:20 +0300

    feat: change solution for task 2

--- 6. Conditions/02/script.ts
+++ 6. Conditions/02/script.ts
@@ -1,15 +1,24 @@
 // Write a JavaScript conditional statement to find the sign of
 // product of three numbers. Display an alert box with the specified sign.
 
-function sign(a: number, b: number, c: number) {
-  const product = a * b * c;
-  return product < 0 ? "-" : "+";
+function sign(x: number, y: number, z: number) {
+  if (x > 0 && y > 0 && z > 0) {
+    return "The sign is +";
+  } else if (x < 0 && y < 0 && z < 0) {
+    return "The sign is -";
+  } else if (x > 0 && y < 0 && z < 0) {
+    return "The sign is +";
+  } else if (x < 0 && y > 0 && z < 0) {
+    return "The sign is +";
+  } else {
+    return "The sign is -";
+  }
 }
 
 console.log(sign(30, 40, -10));
 console.log(sign(30, -10, -20));
 console.log(sign(-54, -21, -10));
 
-// -
-// +
-// -
+// The sign is -
+// The sign is +
+// The sign is -
