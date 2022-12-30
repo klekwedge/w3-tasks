2ce0f27db9132446ebeb41abec07789fa0700229
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 13:30:56 +0300

    feat: add a solution to task number 20

---
+++ 10. String/20/script.js
@@ -0,0 +1,21 @@
+"use strict";
+// Write a JavaScript function that can pad (left, right) a string to get
+// to a determined length. Go to the editor
+// Test Data :
+// console.log(formatted_string('0000',123,'l'));
+// console.log(formatted_string('00000000',123,''));
+// Output :
+// "0123"
+// "12300000"
+function formattedString(str, num, side) {
+    if (side) {
+        return str.slice(0, String(num).length - 2) + num;
+    }
+    else {
+        return num + str.slice(0, String(num).length + 2);
+    }
+}
+console.log(formattedString("0000", 123, "l"));
+console.log(formattedString("00000000", 123, ""));
+// 0123
+// 12300000
