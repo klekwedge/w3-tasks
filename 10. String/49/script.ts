42b023f964a69938aeeb69a930404b4b6fd5a26a
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 15:22:10 +0300

    feat: add a solution to task number 49

---
+++ 10. String/49/script.ts
@@ -0,0 +1,24 @@
+// Write a JavaScript function to get unique guid (an acronym for 'Globally Unique Identifier?)
+// of the specified length, or 32 by default.
+// Test Data :
+// console.log(guid());
+// console.log(guid(15));
+// "hRYilcoV7ajokxsYFl1dba41AyE0rUQR"
+// "b7pwBqrZwqaDrex"
+
+function guid(len: number = 32) {
+  const res = [];
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < len; i++) {
+    res[i] = chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return res.join("");
+}
+
+console.log(guid());
+console.log(guid(15));
+
+// MnwwQkI8nPjilJtJFx4WhYEKgqfXTiPt
+// wdNewqrt7jmsMzY
