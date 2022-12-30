93ba462870c0375836c9fa6432d1b691a5de9f70
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 12:23:27 +0300

    feat: add description of test data for 1 task

--- 10. String/01/script.ts
+++ 10. String/01/script.ts
@@ -1,4 +1,9 @@
 // Write a JavaScript function to check whether an `input` is a string or not.
+// Test Data :
+// console.log(is_string('w3resource'));
+// true
+// console.log(is_string([1, 2, 4, 0]));
+// false
 
 function isString(input: any) {
   return typeof input === "string";
