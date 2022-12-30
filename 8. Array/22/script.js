c3ca18bf582e5df47696e4ed4a825a670fa48e9d
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 17:44:26 +0300

    feat: add a solution to task number 22

---
+++ 8. Array/22/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// Write a JavaScript program to compute the union of two arrays.
+// Sample Data :
+// console.log(union([1, 2, 3], [100, 2, 1, 10]));
+// [1, 2, 3, 10, 100]
+function union(arr1, arr2) {
+    return [...new Set([...arr1, ...arr2])].sort((a, b) => a - b);
+}
+console.log(union([1, 2, 3], [100, 2, 1, 10]));
+// [ 1, 2, 3, 10, 100 ]
