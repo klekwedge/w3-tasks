673e12da3d646c256de6435726c337499e4bdf68
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 11:24:08 +0300

    feat: add a solution to task number 13

---
+++ 4. Functions/13/script.ts
@@ -0,0 +1,23 @@
+// Write a JavaScript function to compute the factors of a positive integer.
+
+function factors(num: number) {
+  const numFactors = [];
+
+  for (let i = 1; i <= Math.floor(Math.sqrt(num)); i += 1)
+    if (num % i === 0) {
+      numFactors.push(i);
+      if (num / i !== i) {
+        numFactors.push(num / i);
+      }
+    }
+
+  return numFactors.sort((a, b) => a - b);
+}
+
+console.log(factors(15));
+console.log(factors(16));
+console.log(factors(17));
+
+// [ 1, 3, 5, 15 ]
+// [ 1, 2, 4, 8, 16 ]
+// [ 1, 17 ]
