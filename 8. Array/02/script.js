5f52fa40e037d3aaa237acc99cea8385d8ce1df0
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 16:04:04 +0300

    feat: add a solution to task number 2

---
+++ 8. Array/02/script.js
@@ -0,0 +1,14 @@
+"use strict";
+// Write a JavaScript function to clone an array.
+// Test Data :
+// console.log(array_Clone([1, 2, 4, 0]));
+// console.log(array_Clone([1, 2, [4, 0]]));
+// [1, 2, 4, 0]
+// [1, 2, [4, 0]]
+function arrayClone(arr) {
+    return JSON.parse(JSON.stringify(arr));
+}
+console.log(arrayClone([1, 2, 4, 0]));
+console.log(arrayClone([1, 2, [4, 0]]));
+// [ 1, 2, 4, 0 ]
+// [ 1, 2, [ 4, 0 ] ]
