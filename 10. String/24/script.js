77ffa3eddf0cc917d6ef607fdf4642421063d7d3
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 13:46:12 +0300

    feat: add a solution to task number 24

---
+++ 10. String/24/script.js
@@ -0,0 +1,11 @@
+"use strict";
+// Write a JavaScript function to truncate a string to a certain number of words.
+// Test Data :
+// console.log(truncate('The quick brown fox jumps over the lazy dog', 4));
+// Output :
+// "The quick brown fox"
+function truncate(str, num) {
+    return str.split(" ").filter((item, i) => i < num).join(' ');
+}
+console.log(truncate("The quick brown fox jumps over the lazy dog", 4));
+// The quick brown fox
