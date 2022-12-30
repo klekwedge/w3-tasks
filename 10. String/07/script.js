fb530d50b11d6572f8e412264ad8bd5d46b6480a
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 12:27:50 +0300

    feat: change job numbering

---
+++ 10. String/07/script.js
@@ -0,0 +1,11 @@
+"use strict";
+// Write a JavaScript function to parameterize a string.
+// Test Data :
+// console.log(string_parameterize("Robin Singh from USA."));
+// "robin-singh-from-usa"
+function stringParameterize(str) {
+    str = str.toLowerCase().replace(".", "");
+    return str.split(" ").join("-");
+}
+console.log(stringParameterize("Robin Singh from USA."));
+// robin-singh-from-usa
