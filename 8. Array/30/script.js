8b93b1575b07eca2938ace096d9d68cc07c2c57c
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 18:00:27 +0300

    feat: add a solution to task number 30

---
+++ 8. Array/30/script.js
@@ -0,0 +1,15 @@
+"use strict";
+// Write a JavaScript function to merge two arrays and
+// removes all duplicates elements.
+// Test data :
+// var array1 = [1, 2, 3];
+// var array2 = [2, 30, 1];
+// console.log(merge_array(array1, array2));
+// [3, 2, 30, 1]
+function mergeArray(a, b) {
+    return [...new Set([...a, ...b])];
+}
+const array1 = [1, 2, 3];
+const array2 = [2, 30, 1];
+console.log(mergeArray(array1, array2));
+// [ 1, 2, 3, 30 ]
