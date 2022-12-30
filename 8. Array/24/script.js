8fc2b56649531bae691d4e4ddfd2e87915d2027b
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 17:47:00 +0300

    feat: add a solution to task number 24

---
+++ 8. Array/24/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// Write a JavaScript function to remove. 'null', '0', '""', 'false',
+// 'undefined' and 'NaN' values from an array.
+// Sample array : [NaN, 0, 15, false, -22, '',undefined, 47, null]
+// Expected result : [15, -22, 47]
+function filterArray(arr) {
+    return arr.filter((item) => item);
+}
+console.log(filterArray([NaN, 0, 15, false, -22, "", undefined, 47, null]));
+// [15,-22,47]
