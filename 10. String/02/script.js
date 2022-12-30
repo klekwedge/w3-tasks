c8684669cec055a27ae8d7bc49f1feb3233d1fbb
Andrey <antonov.andrey.10101@gmail.com>
2022-12-30 12:12:17 +0300

    feat: add a solution to task number 2

---
+++ 10. String/02/script.js
@@ -0,0 +1,9 @@
+"use strict";
+// Write a JavaScript function to check whether a string is blank or not.
+function isBlank(str) {
+    return str.length === 0;
+}
+console.log(isBlank(""));
+console.log(isBlank("abc"));
+// true
+// false
---
+++ 10. String/02/script.ts
@@ -0,0 +1,10 @@
+// Write a JavaScript function to check whether a string is blank or not.
+
+function isBlank(str: string) {
+  return str.length === 0;
+}
+console.log(isBlank(""));
+console.log(isBlank("abc"));
+
+// true
+// false
