0017c1ffeac4a3af0565e265e88ec53978acc26e
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 10:54:31 +0300

    feat: add a solution to task number 24

---
+++ 2. Fundamental 1/24/script.js
@@ -0,0 +1,13 @@
+"use strict";
+// Write a JavaScript program to filter out the
+// non-unique values in an array, based on a provided
+// comparator function.
+const decapitalize = (str, upper = false) => {
+    return !upper
+        ? str[0].toLowerCase() + str.slice(1)
+        : str[0].toLowerCase() + str.slice(1).toUpperCase();
+};
+console.log(decapitalize("W3resource"));
+console.log(decapitalize("Red", true));
+// w3resource
+// rED
