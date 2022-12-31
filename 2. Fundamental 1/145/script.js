aa7e4d2feb26f1b295fe89b0c4c5f274d3dab176
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 14:21:20 +0300

    feat: add a solution to task number 145

---
+++ 2. Fundamental 1/145/script.js
@@ -0,0 +1,14 @@
+"use strict";
+// Write a JavaScript program to randomize the order
+// of the values of an array, returning a new array.
+const shuffle = (arr) => {
+    const newArr = JSON.parse(JSON.stringify(arr));
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+    }
+    return newArr;
+};
+const foo = [1, 2, 3];
+console.log(shuffle(foo));
+// [ 2, 1, 3 ]
