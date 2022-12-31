c3c184bd7dfa18149b3ef6b1308026b2ebf8a7e6
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:43:06 +0300

    feat: add a solution to task number 59

---
+++ 7. Math/59/script.js
@@ -0,0 +1,12 @@
+"use strict";
+// Write a JavaScript program to calculate the volume of a Pyramid.
+function volumePyramid(baseLength, baseWidth, height) {
+    if (baseLength < 0 || baseWidth < 0 || height < 0) {
+        return null;
+    }
+    return (baseLength * baseWidth * height) / 3.0;
+}
+console.log(volumePyramid(2.0, 3.0, 7.0));
+console.log(volumePyramid(2.0, -3.0, 7.0));
+// 14
+// null
