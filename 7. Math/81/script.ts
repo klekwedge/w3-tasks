cd0fa783967519d10b9836ce07c8fe1319532331
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 09:16:32 +0300

    feat: add a solution to task number 81

---
+++ 7. Math/81/script.ts
@@ -0,0 +1,30 @@
+// Write a JavaScript program to given two non-negative integers num1
+// and num2 represented as string, return the sum of num1 and num2.
+
+function test(num: number) {
+  return String(num)
+    .split("")
+    .map((item) => (+item) ** 3)
+    .join("");
+}
+
+let n = 11;
+console.log(test(n));
+
+n = 66;
+console.log(test(n));
+
+n = 336;
+console.log(test(n));
+
+n = 444;
+console.log(test(n));
+
+n = 1151;
+console.log(test(n));
+
+// 11
+// 216216
+// 2727216
+// 646464
+// 111251
