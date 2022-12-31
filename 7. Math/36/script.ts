cbad3802c259704f303b9e8497f789c367aeb908
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:40:52 +0300

    feat: add a solution to task number 36

---
+++ 7. Math/36/script.ts
@@ -0,0 +1,29 @@
+// Write a JavaScript function which will return values that are powers of two.
+// Test Data :
+// console.log(isPower_of_two(64));
+// true
+// console.log(isPower_of_two(94));
+// false
+
+function isPowerOfTwo(num: number) {
+  const buff = num;
+
+  if (num === 1 || num === 2) {
+    return true;
+  }
+
+  let count = 1;
+
+  while (num % 2 === 0) {
+    count *= 2;
+    num /= 2;
+  }
+
+  return count === buff;
+}
+
+console.log(isPowerOfTwo(64));
+console.log(isPowerOfTwo(94));
+
+// true
+// false
