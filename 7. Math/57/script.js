e7699253177af399536defd47ee123b54bb9d1cf
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:42:45 +0300

    feat: add a solution to task number 57

---
+++ 7. Math/57/script.js
@@ -0,0 +1,12 @@
+"use strict";
+// Write a JavaScript program to calculate the volume of a Cube.
+function volumeCube(length) {
+    if (length < 0) {
+        return null;
+    }
+    return length ** 3;
+}
+console.log(volumeCube(3.0));
+console.log(volumeCube(-3.0));
+// 24
+// null
