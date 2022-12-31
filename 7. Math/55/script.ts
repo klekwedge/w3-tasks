29a997ad3632dc9e3d4ca8d9626e53551a4ad049
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:42:21 +0300

    feat: add a solution to task number 55

---
+++ 7. Math/55/script.ts
@@ -0,0 +1,13 @@
+// Write a JavaScript program to swap two given variables.
+// Swapping two variables refers to mutually exchanging the
+// values of the variables. Generally, this is done with
+// the data in memory.
+
+let x = 20;
+let y = 40;
+
+[x, y] = [y, x];
+
+console.log(x, y);
+
+// 40 20
