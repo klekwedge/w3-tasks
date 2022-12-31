e05c59f9b6056c964d3326127d9b31cd1d39976c
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 11:45:13 +0300

    feat: add a solution to task number 92

---
+++ 2. Fundamental 1/92/script.js
@@ -0,0 +1,11 @@
+"use strict";
+// Write a JavaScript program to create a function that gets
+// the argument at index n. If n is negative, the nth argument
+// from the end is returned.
+const nthArg = (arr, pos) => pos > 0 ? arr[pos] : arr[arr.length - 1];
+console.log(nthArg([1, 2, 3], 2));
+console.log(nthArg([1, 2], 2));
+console.log(nthArg([1, 2, 3, 4, 5], -1));
+// 3
+// undefined
+// 5
