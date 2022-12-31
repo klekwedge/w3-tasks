6e00bc9b7c810a4a3de2d06bad9bfa4f4a315e0d
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:37:29 +0300

    feat: add a solution to task number 7

---
+++ 7. Math/07/script.ts
@@ -0,0 +1,16 @@
+// Write a JavaScript function to find the lowest value in an array.
+// Test Data :
+// console.log(min([12,34,56,1]));
+// console.log(min([-12,-34,0,-56,-1]));
+// 1
+// -56
+
+function min(arr: number[]) {
+  return Math.min(...arr);
+}
+
+console.log(min([12,34,56,1]));
+console.log(min([-12,-34,0,-56,-1]));
+
+// 1
+// -56
