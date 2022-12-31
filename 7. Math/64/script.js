2ca4dba99f27159d6a7d0de30c3b707a750cd80e
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:43:59 +0300

    feat: add a solution to task number 64

---
+++ 7. Math/64/script.js
@@ -0,0 +1,9 @@
+"use strict";
+//  Write a JavaScript program to calculate the volume of a Hemisphere.
+function volumeHemisphere(radius) {
+    return radius < 0 ? null : (2.0 * Math.PI * radius ** 3) / 3.0;
+}
+console.log(volumeHemisphere(4.0));
+console.log(volumeHemisphere(-4.0));
+// 134.0412865531645
+// null
