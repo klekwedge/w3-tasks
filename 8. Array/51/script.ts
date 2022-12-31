93c349f8fc96d3a8a0bb29018ce104e8b04be109
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:32:54 +0300

    feat: add a solution to task number 51

---
+++ 8. Array/51/script.ts
@@ -0,0 +1,45 @@
+// Write a JavaScript program to check if an array is a factor chain or not.
+// A factor chain is an array in which the previous element is a factor of
+// the next consecutive element. The following is a factor chain:
+// [2, 4, 8, 16, 32]
+// 2 is a factor of 4
+// 4 is a factor of 8
+// 8 is a factor of 16
+// 16 is a factor of 32
+
+// Test Data :
+// ([2, 4, 8, 16, 32]) -> true
+// ([2, 4, 16, 32, 64]) -> true
+// ([2, 4, 16, 32, 68]) -> false
+// Expected Output:
+// Original array:
+// Check the said array is a factor chain or not?
+// true
+// Original array:
+// Check the said array is a factor chain or not?
+// true
+// Original array:
+// Check the said array is a factor chain or not?
+// false
+
+function test(arr: number[]) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] % arr[i - 1] !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let nums = [2, 4, 8, 16, 32];
+console.log(test(nums));
+
+nums = [2, 4, 16, 32, 64];
+console.log(test(nums));
+
+nums = [2, 4, 16, 32, 68];
+console.log(test(nums));
+
+// true
+// true
+// false
