8902959b3ebb7bd1d2eeb3f6236fc252e348321a
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:39:44 +0300

    feat: add a solution to task number 30

---
+++ 7. Math/30/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// Write a JavaScript function to cast a square root of a number to an integer.
+// Test Data :
+// console.log(sqrt_to_int(17));
+// 4
+function sqrtToInt(num) {
+    return Math.floor(Math.sqrt(num));
+}
+console.log(sqrtToInt(17));
+// 4
---
+++ 7. Math/30/script.ts
@@ -0,0 +1,12 @@
+// Write a JavaScript function to cast a square root of a number to an integer.
+// Test Data :
+// console.log(sqrt_to_int(17));
+// 4
+
+function sqrtToInt(num: number) {
+  return Math.floor(Math.sqrt(num))
+}
+
+console.log(sqrtToInt(17));
+
+// 4
