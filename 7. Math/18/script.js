0ed4eb610df8d787065aee77821a062511f4f381
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:38:43 +0300

    feat: add a solution to task number 18

---
+++ 7. Math/18/script.js
@@ -0,0 +1,21 @@
+"use strict";
+// Write a JavaScript function to calculate the product of values in an array.
+// Test Data :
+// console.log(product([1,2,3]));
+// console.log(product([100,-200,3]));
+// console.log(product([1,2,'a',3]));
+// Output :
+// 6
+// -60000
+// 6
+function product(arr) {
+    return arr
+        .filter((item) => typeof item === "number")
+        .reduce((prev, next) => prev * next);
+}
+console.log(product([1, 2, 3]));
+console.log(product([100, -200, 3]));
+console.log(product([1, 2, "a", 3]));
+// 6
+// -60000
+// 6
