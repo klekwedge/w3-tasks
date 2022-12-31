3c4a4778aa8532f554eba838df1e380082a34319
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 10:03:33 +0300

    feat: add a solution to task number 92

---
+++ 7. Math/92/script.ts
@@ -0,0 +1,32 @@
+// Write a program that takes a positive integer and
+// calculate the cube root of the number until the number
+// is less than three. Return the number of steps to
+// complete the said process.
+// Test Data:
+// (27) -> 2
+// (10000) -> 2
+// (-100) -> "Not a positive number!"
+
+function test(num: number) {
+  let counter = 0;
+
+  while (num >= 3) {
+    num = num ** (1 / 3);
+    counter++;
+  }
+
+  return counter;
+}
+
+let n = 27;
+console.log(test(n));
+
+n = 10000;
+console.log(test(n));
+
+n = -100;
+console.log(test(n));
+
+// 2
+// 2
+// 0
