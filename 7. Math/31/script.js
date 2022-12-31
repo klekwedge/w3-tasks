6247f15f5b0051be066f74da53d496784bf7fe8d
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:39:55 +0300

    feat: add a solution to task number 31

---
+++ 7. Math/31/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// Write a JavaScript function to get the highest number from three different numbers.
+// Test Data :
+// console.log(highest_of_three(-5, 4, 2));
+// 4
+function highestOfThree(a, b, c) {
+    return Math.max(a, b, c);
+}
+console.log(highestOfThree(-5, 4, 2));
+// 4
---
+++ 7. Math/31/script.ts
@@ -0,0 +1,12 @@
+// Write a JavaScript function to get the highest number from three different numbers.
+// Test Data :
+// console.log(highest_of_three(-5, 4, 2));
+// 4
+
+function highestOfThree(a: number, b: number, c: number) {
+  return Math.max(a, b, c);
+}
+
+console.log(highestOfThree(-5, 4, 2));
+
+// 4
