ab26ce66744860f1369ebbe2319958b9663fad17
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:32:18 +0300

    feat: add a solution to task number 45

---
+++ 8. Array/45/script.ts
@@ -0,0 +1,21 @@
+// Write a JavaScript program to find all unique values in an given array of numbers.
+// Test Data :
+// [1, 2, 2, 3, 4, 4, 5]
+// [1, 2, 3, 4, 5]
+// [1, -2, -2, 3, 4, -5, -6, -5]
+// Expected Output:
+// [1,2,3,4,5]
+// [1,2,3,4,5]
+// [1,-2,3,4,-5,-6]
+
+function uniqueElements(arr: number[]) {
+  return [...new Set(arr)];
+}
+
+console.log(uniqueElements([1, 2, 2, 3, 4, 4, 5]));
+console.log(uniqueElements([1, 2, 3, 4, 5]));
+console.log(uniqueElements([1, -2, -2, 3, 4, -5, -6, -5]));
+
+// [ 1, 2, 3, 4, 5 ]
+// [ 1, 2, 3, 4, 5 ]
+// [ 1, -2, 3, 4, -5, -6 ]
