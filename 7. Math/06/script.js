03ce767b5d264e2cb990372b590b861050a09d3e
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:37:19 +0300

    feat: add a solution to task number 6

---
+++ 7. Math/06/script.js
@@ -0,0 +1,14 @@
+"use strict";
+// Write a JavaScript function to find the highest value in an array.
+// Test Data :
+// console.log(max([12,34,56,1]));
+// console.log(max([-12,-34,0,-56,-1]));
+// 56
+// 0
+function max(arr) {
+    return Math.max(...arr);
+}
+console.log(max([12, 34, 56, 1]));
+console.log(max([-12, -34, 0, -56, -1]));
+// 56
+// 0
