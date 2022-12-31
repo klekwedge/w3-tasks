3521286e094e9f51211efbbf46ffc16a8cabc9d9
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:40:17 +0300

    feat: add a solution to task number 33

---
+++ 7. Math/33/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// Write a JavaScript function to convert an angle from degrees to radians.
+// Test Data :
+// console.log(degrees_to_radians(45));
+// 0.7853981633974483
+function degreesToRadians(num) {
+    return (num * Math.PI) / 180;
+}
+console.log(degreesToRadians(45));
+// 0.7853981633974483
