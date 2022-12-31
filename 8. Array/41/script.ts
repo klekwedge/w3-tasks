127d30affa4caf166344cb817e7bb04147771f22
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:32:02 +0300

    feat: add a solution to task number 41

---
+++ 8. Array/41/script.ts
@@ -0,0 +1,27 @@
+// Write a JavaScript function to generate an array between two integers of 1 step length.
+// Test Data :
+// console.log(rangeBetwee(4, 7));
+// [4, 5, 6, 7]
+// console.log(rangeBetwee(-4, 7));
+// [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
+
+function rangeBetwee(start: number, end: number) {
+  const res = [];
+
+  for (let i = start; i <= end; i++) {
+    res.push(i);
+  }
+
+  return res;
+}
+
+console.log(rangeBetwee(4, 7));
+console.log(rangeBetwee(-4, 7));
+
+// [ 4, 5, 6, 7 ]
+
+// [
+//   -4, -3, -2, -1, 0,
+//    1,  2,  3,  4, 5,
+//    6,  7
+// ]
