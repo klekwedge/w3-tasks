22b688d1dad5f11fb64afb57d5d5da2733f7d440
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 10:33:04 +0300

    feat: add a solution to task number 8

---
+++ 2. Fundamental 1/08/script.ts
@@ -0,0 +1,15 @@
+// Write a JavaScript program to converts a specified number
+// to an array of digits.
+
+function pull(arr: string[], ...elements: string[]) {
+  return arr.filter((item) => !elements.includes(item));
+}
+
+let arra1 = ["a", "b", "c", "a", "b", "c"];
+console.log(pull(arra1, "a", "c"));
+
+let arra2 = ["a", "b", "c", "a", "b", "c"];
+console.log(pull(arra2, "b"));
+
+// [ 'b', 'b' ]
+// [ 'a', 'c', 'a', 'c' ]
