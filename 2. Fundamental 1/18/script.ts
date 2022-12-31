6fe2140976c20205a900d2af4f7f9ec7cde34088
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 10:43:53 +0300

    feat: change the numbering of one task

---
+++ 2. Fundamental 1/18/script.ts
@@ -0,0 +1,16 @@
+// Write a JavaScript program to remove specified elements
+// from the left of a given array of elements.
+
+const removeFromLeft = (arr: number[], pos: number = 1) => {
+  return arr.slice(pos);
+};
+
+console.log(removeFromLeft([1, 2, 3]));
+console.log(removeFromLeft([1, 2, 3], 1));
+console.log(removeFromLeft([1, 2, 3], 2));
+console.log(removeFromLeft([1, 2, 3], 4));
+
+// [ 2, 3 ]
+// [ 2, 3 ]
+// [ 3 ]
+// []
