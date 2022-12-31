d227d10fe62293a08b1d3725158b41fc684a85ef
Andrey <antonov.andrey.10101@gmail.com>
2022-12-31 08:43:39 +0300

    feat: add a solution to task number 62

---
+++ 7. Math/62/script.ts
@@ -0,0 +1,22 @@
+// Write a JavaScript program to calculate the volume of
+// a prism using only its height and one of its hexagonal sides.
+
+function volumePentagonalPrism(baseEdge: number, height: number) {
+  if (baseEdge < 0 || height < 0) {
+    return null;
+  }
+  return (
+    (1 / 4) *
+    height *
+    baseEdge *
+    baseEdge *
+    Math.sqrt(5 * (5 + 2 * Math.sqrt(5)))
+  );
+}
+
+console.log(volumePentagonalPrism(4.0, 8.0));
+console.log(volumePentagonalPrism(4.0, -8.0));
+
+// 220.22110727538777
+// 495.4974913696225
+// null
